bd6aaf3b205da43e9fc9a34d099de7d467242542
Andrea <aandreani1998@gmail.com>
2024-10-22 14:22:34 +0200

    stable lua config

---
+++ docs.md
@@ -0,0 +1,15 @@
+# NVIM DOCS
+
+## None LS
+
+None ls wraps up formatter and linter commands into lsp-like commands!
+
+## Clean up
+
+Remove the following:
+
+~/.config/nvim
+~/.local/share/nvim
+
+- [ ] Prova
+- [x] Prova 2
---
+++ lua/vim-options.lua
@@ -0,0 +1,14 @@
+-- Tab options
+vim.cmd("set expandtab")
+vim.cmd("set tabstop=2")
+vim.cmd("set softtabstop=2")
+vim.cmd("set shiftwidth=2")
+
+-- Window Options
+vim.cmd("set colorcolumn=80")
+
+-- Search Options
+vim.cmd("set nohlsearch")
+
+-- Set leader to space
+vim.g.mapleader = " "
