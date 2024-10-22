f40334a0e224e135cba24b2e21b4171dbf70f347
Andrea <aandreani1998@gmail.com>
2024-10-22 20:09:51 +0200

    Added keybinds to scroll through cmp options

--- lua/plugins/completions.lua
+++ lua/plugins/completions.lua
@@ -30,11 +30,13 @@ return {
           documentation = cmp.config.window.bordered(),
         },
         mapping = cmp.mapping.preset.insert({
-          ["<C-b>"] = cmp.mapping.scroll_docs(-4),
+          ["<C-d>"] = cmp.mapping.scroll_docs(-4),
           ["<C-f>"] = cmp.mapping.scroll_docs(4),
           ["<C-Space>"] = cmp.mapping.complete(),
           ["<C-e>"] = cmp.mapping.abort(),
           ["<CR>"] = cmp.mapping.confirm({ select = true }),
+          ["<C-j>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Select }),
+          ["<C-k>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Select }),
         }),
         sources = cmp.config.sources({
           { name = "nvim_lsp" },
